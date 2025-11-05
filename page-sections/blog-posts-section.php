6fa3ad2793f724b33e8593c6586283a74aa7dd30
joy-passion <joy@passion-productions.co.uk>
2025-11-05 11:56:41 +0000

    Added blog posts section

--- page-sections/blog-posts-section.php
+++ page-sections/blog-posts-section.php
@@ -37,11 +37,24 @@ if ( ! defined( 'ABSPATH' ) ) {
             
             <!--  Blog Posts -->
             <?php if( $blog_posts ): ?>
-                <ul>
+                <ul class="blog-posts">
                 <?php foreach( $blog_posts as $blog_post ):  
-                    setup_postdata( $blog_post ); ?>
-                    <li>
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    $permalink = get_permalink( $blog_post->ID );
+                    $title = get_the_title( $blog_post->ID );
+                    $featured_image = get_the_post_thumbnail( $blog_post->ID, 'medium' );
+                ?>
+                    <li class="blog-post blog-post--small">
+                        <?php if ( $featured_image ) : ?>
+                            <a href="<?php echo esc_url( $permalink ); ?>" class="blog-post__image">
+                                <?php echo $featured_image; ?>
+                            </a>
+                        <?php endif; ?>
+                        <div class="blog-post__content">
+                            <h3><?php echo esc_html( $title ); ?></h3>
+                            <a href="<?php echo esc_url( $permalink ); ?>" class="btn btn--primary">
+                                Read More
+                            </a>
+                        </div>
                     </li>
                 <?php endforeach; ?>
                 </ul>
