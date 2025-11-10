4bd523e48347f0d9100ff7751622a98f166e34fa
joy-passion <joy@passion-productions.co.uk>
2025-11-10 09:58:40 +0000

    Added business information shortcodes

--- functions/shortcode-functions.php
+++ functions/shortcode-functions.php
@@ -32,19 +32,59 @@ function mytheme_social_media_shortcode( $atts ) {
 add_shortcode( 'social_media', 'mytheme_social_media_shortcode' );
 
 /**
+ * Shortcode: [business_name]
+ * Description: Displays the business name as defined in the ACF options page
+ */
+function display_business_name() {
+    $name = get_field('business_name', 'option');
+
+    if (empty($name)) {
+        return '';
+    }
+
+    return wp_kses_post($name);
+}
+add_shortcode('business_name', 'display_business_name');
+
+/**
  * Shortcode: [business_address]
  * Description: Displays the business address as defined in the ACF options page
  */
 function display_business_address() {
-    // Get the ACF field from the options page
     $address = get_field('business_address', 'option');
-
-    // If the field is empty, return nothing
     if (empty($address)) {
         return '';
     }
-
-    // Return safely escaped address
     return wp_kses_post($address);
 }
 add_shortcode('business_address', 'display_business_address');
+
+/**
+ * Shortcode: [business_phone_number]
+ * Description: Displays the business phone number as defined in the ACF options page
+ */
+function display_business_phone_number() {
+    $phone_number = get_field('business_phone_number', 'option');
+
+    if (empty($phone_number)) {
+        return '';
+    }
+
+    return '<a href="tel:'.esc_attr($phone_number).'">'.esc_html($phone_number).'</a>';
+}
+add_shortcode('business_phone_number', 'display_business_phone_number');
+
+/**
+ * Shortcode: [business_email]
+ * Description: Displays the business email as defined in the ACF options page
+ */
+function display_business_email() {
+    $email = get_field('business_email', 'option');
+
+    if (empty($email)) {
+        return '';
+    }
+
+    return '<a href="mailto:'.esc_attr($email).'">'.esc_html($email).'</a>';
+}
+add_shortcode('business_email', 'display_business_email');
