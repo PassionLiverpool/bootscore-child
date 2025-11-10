f13bba0d896c4e5d2fbe70ad23e8dd18880c8381
joy-passion <joy@passion-productions.co.uk>
2025-11-10 13:55:25 +0000

    Added top padding to categories list

--- page-templates/blog.php
+++ page-templates/blog.php
@@ -26,6 +26,31 @@ get_header();
           <!-- Page banner -->
           <?php include get_stylesheet_directory() . '/components/hero-banner.php'; ?>
 
+          <!-- Category List -->
+          <div class="categories container">
+            <ul class="categories__list">
+              <li class="categories__item">
+                <a href="/our-blog">All</a>
+              </li>
+              <?php
+              $categories = get_categories([
+                  'orderby' => 'name',
+                  'order'   => 'ASC',
+                  'hide_empty' => true,
+              ]);
+
+              foreach ($categories as $category) :
+                  $category_link = get_category_link($category->term_id);
+                  ?>
+                  <li class="categories__item">
+                    <a href="<?php echo esc_url($category_link); ?>" class="blog-categories__link">
+                      <?php echo esc_html($category->name); ?>
+                    </a>
+                  </li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+
           <!-- Blog Posts -->
           <div class="blog-archive">
             <div class="container">
