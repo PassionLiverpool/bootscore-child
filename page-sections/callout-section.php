cf44a503e000a77618cf8224d39815ea664b8be1
joy-passion <joy@passion-productions.co.uk>
2025-11-10 15:45:32 +0000

    Creating callout section

---
+++ page-sections/callout-section.php
@@ -0,0 +1,99 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+    $content = get_sub_field('section_content');
+
+    // Text
+    $header = $content['header'] ?? '';
+    $header_style = $content['header_style'] ?? 'h2';
+    $wysiwyg_text = $content['wysiwyg_text'];
+
+    // Media
+    $media_type = $content['media_type'] ?? 'none';
+    $image = $content['image'] ?? '';
+    $content_video = $content['video'] ?? '';
+    $content_video_thumbnail = $content['video_thumbnail'] ?? '';
+
+    // Buttons
+    $primary_button = $content['primary_button'];
+    $secondary_button = $content['secondary_button'];
+
+    // Appearance
+    $callout_section_style = get_sub_field('callout_section_style') ?? 'media-bottom';
+    $section_appearance = get_sub_field('section_appearance');
+    $background_color = $section_appearance['background_colour'];
+    $padding_top = $section_appearance['padding_top'];
+    $padding_bottom = $section_appearance['padding_bottom'];
+
+    include get_stylesheet_directory() . '/components/content-settings.php';
+?>
+
+<section class="callout-section "
+         <?php if($html_id): ?>id="<?php echo $html_id; ?>"<?php endif; ?>
+         style="padding-top: <?php echo $padding_top ?>rem; padding-bottom: <?php echo $padding_bottom ?>rem"
+>
+    <div class="container background--<?php echo $background_color ?>">
+
+        <!-- Text content -->
+        <div class="callout-section__text">
+            <!-- Header -->
+            <?php if ( $header ) : ?>
+                <<?php echo esc_attr( $header_style ); ?> class="callout-section__header">
+                    <?php echo esc_html( $header ); ?>
+                </<?php echo esc_attr( $header_style ); ?>>
+            <?php endif; ?>
+
+            <!-- WYSIWYG -->
+            <?php if ( $wysiwyg_text ) : ?>
+                <div class="callout-section__wysiwyg">
+                    <?php echo wp_kses_post( $wysiwyg_text ); ?>
+                </div>
+            <?php endif; ?>
+
+            <!-- Buttons -->
+            <?php if($primary_button || $secondary_button): ?>
+                <div class="callout-section__buttons">
+                    <?php if($primary_button): ?>
+                        <?php
+                            $button = $primary_button;
+                            include get_stylesheet_directory() . '/components/button.php';
+                        ?>
+                    <?php endif; ?>
+                    <?php if($secondary_button): ?>
+                        <?php
+                            $button = $secondary_button;
+                            include get_stylesheet_directory() . '/components/button.php';
+                        ?>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+
+        </div>
+        
+        <!-- Media -->
+        <?php if($image || $content_video): ?>
+            <div class="callout-section__media">
+                <!-- Image -->
+                <?php if($media_type == "image"): ?>
+                    <div class="callout-section__image">
+                        <?php echo wp_get_attachment_image($image['id'], 'medium', false, array('loading'=>'lazy')); ?>
+                    </div>
+                <?php endif; ?>
+
+                <!-- Video -->
+                 <?php
+                 if($media_type == "video"):
+                ?>
+                    <div class="callout-section__video">
+                        <?php
+                            $video = $content_video;
+                            $video_thumbnail = $content_video_thumbnail;
+                            include get_stylesheet_directory() . '/components/video.php';
+                        ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        <?php endif; ?>
+    </div>
+</section>
