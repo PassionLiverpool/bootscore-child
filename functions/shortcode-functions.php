9b188ef325959400a8c6947184892ffcd50bfa15
joy-passion <joy@passion-productions.co.uk>
2025-11-10 09:51:04 +0000

    Add business address shortcode

--- functions/shortcode-functions.php
+++ functions/shortcode-functions.php
@@ -30,3 +30,21 @@ function mytheme_social_media_shortcode( $atts ) {
     return ob_get_clean(); // Return the buffered content
 }
 add_shortcode( 'social_media', 'mytheme_social_media_shortcode' );
+
+/**
+ * Shortcode: [business_address]
+ * Description: Displays the business address as defined in the ACF options page
+ */
+function display_business_address() {
+    // Get the ACF field from the options page
+    $address = get_field('business_address', 'option');
+
+    // If the field is empty, return nothing
+    if (empty($address)) {
+        return '';
+    }
+
+    // Return safely escaped address
+    return wp_kses_post($address);
+}
+add_shortcode('business_address', 'display_business_address');
